97addf92626ae10709cfeb52f4d668a04fe5c96f
Bill Tozier <bill@vagueinnovation.com>
2016-01-19 09:35:50 -0500

    cleaned up plush translator

--- src/answer_factory/genomes/plush.clj
+++ src/answer_factory/genomes/plush.clj
@@ -1,17 +1,87 @@
 (ns answer-factory.genomes.plush
-  (:require [clojure.zip :as zip]))
+  (:require [clojure.zip :as zip])
+  (:require [answer-factory.genomes.bb8 :as bb8]))
 
 
-;; the loop is initialized with
-;; - depth = 0
-;; - openings = 0
-;; - closings = 0
-;;
 
-(defn plush->push
-  "translates"
-  ([genome]
-    (plush->push genome {}))
-  ([genome branch-map]
-  genome))
+(defn clean-insert
+  "inserts the given item at the current cursor position in the indicated zipper; if the cursor is in an empty sub-list, it removes the `nil` placeholder"
+  [z item]
+  (if (nil? (zip/node z))
+    (zip/replace z item)
+    (-> z (zip/insert-right item) zip/right)))
+
+
+(defn empty-program
+  "returns a seq-zip on an empty list, with the cursor advanced into the list"
+  []
+  (zip/next (zip/seq-zip '())))
+
+
+(defn append-token
+  [z item]
+  (clean-insert z item))
+
+
+(defn append-as-branch
+  [z item]
+  (-> (clean-insert z '())
+      zip/next
+      (clean-insert item)))
+
+
+(defn move-up-safely
+  [program]
+  (if (bb8/root? (zip/up program))
+      (zip/rightmost program)
+      (-> (zip/up program)
+          zip/rightmost)))
+
+
+(defn append-as-sibling
+  [z item]
+  (-> (move-up-safely z)
+      (clean-insert '())
+      zip/down
+      (clean-insert item)))
+
+
+
+
+; - increment close-count
+; - (noop_open-paren)
+;   - create a branch (push :END)
+; - noop_delete_prev_paren_pair
+;   - ?
+; - item is in branching table
+;   - push new branch stuff onto branch stack
+; - append an item
+; - consume closes if (pos? close-count)
+;   - move up, consuming branches, that many times
+;   - e.g. [:END :BRANCH :END]
+
+
+; 1. check for tree-editing instructions
+; 2. implement branching items
+; 3. implement boring items
+; 4. do "closing"
+
+
+
+
+
+(defn close-up-one
+  [[program branch-stack]]
+  (let [outcome (first branch-stack)]
+    (vector
+      (cond
+        (nil? outcome) program
+        (= outcome :BRANCH)
+          (-> (move-up-safely program) (zip/insert-right '()) zip/right zip/down)
+        :else (move-up-safely program))
+    (drop 1 branch-stack))))
+
+
+
+
 
--- test/answer_factory/genomes/plush_test.clj
+++ test/answer_factory/genomes/plush_test.clj
@@ -1,6 +1,8 @@
 (ns answer-factory.genomes.plush-test
   (:use midje.sweet)
   (:use answer-factory.genomes.plush)
+  (:require [clojure.zip :as zip])
+  (:require [answer-factory.genomes.bb8 :as bb8])
   (:use clojure.pprint))
 
 
@@ -13,32 +15,124 @@
 ;; :silent metadata
 
 
-
-(fact "translating a genome without a branching map produces a flat program"
-  (plush->push [{:item 1 :close 0}
-                {:item 2 :close 0}
-                {:item 3 :close 0}
-                {:item 4 :close 0}]) => [1 2 3 4]
-  (plush->push []) => []
-  )
+(fact "clean-insert adds a thing to a zipper without leaving the nil placeholder in an empty sub-list"
+  (zip/root (zip/insert-left (zip/next (zip/seq-zip '())) 99)) =>
+    '(99 nil) ;; because of the placeholder nil
+  (zip/root (clean-insert (zip/next (zip/seq-zip '())) 99)) =>
+    '(99))
 
 
-;; some fixtures
-
-(def branch-map-1
-  {:code-quote 1
-   :exec-rot   3
-   :foo        2
-   :bar        0
-   33          1
-   1.234       1})
+(fact "empty-program returns an empty program zipper"
+  (zip/root (empty-program)) => '()
+  (zip/node (empty-program)) => nil
+  (bb8/root? (empty-program)) => false)
 
 
-(fact "translating a genome with branching map creates a branch"
-  (plush->push [{:item 33   :close 0}   ;; BRANCH 1
-                {:item 1    :close 1}
-                {:item 2    :close 0}
-                {:item 3    :close 0}] branch-map-1) => [33 '( 1 ) 2 3]
+;; adding a single item to an existing program
 
-  )
+;; basic cases
+
+(fact "append-token adds an item to the program without branching"
+  (zip/root
+    (append-token (empty-program) 99)) => '(99)
+  (zip/root
+    (append-token (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 99)) =>
+      '(1 2 (3 4 99)))
+
+
+(fact "append-as-branch adds an item to the program in a new branch"
+  (zip/root
+    (append-as-branch (empty-program) 99)) => '((99))
+  (zip/root
+    (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 99)) =>
+      '(1 2 (3 4 (99))))
+
+
+(fact "append-as-sibling adds an item to the program in a new branch"
+  (zip/root
+    (append-as-sibling
+      (append-as-branch (empty-program) 88)
+      99)) => '((88) (99))
+  (zip/root
+    (append-as-sibling
+      (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 88)
+      99)) =>
+      '(1 2 (3 4 (88) (99))))
+
+
+;; genes
+
+;; fixture
+
+(def little-tree
+  (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 99))
+
+
+(fact "a gene with no closes and an empty branch stack"
+  (zip/root
+    (apply-gene [(empty-program) []] {:item :foo :close 0} {})) =>
+      '(:foo)
+  (zip/root
+    (apply-gene [little-tree []] {:item :foo :close 0} {})) =>
+      '(1 2 (3 4 (99 :foo))))
+
+
+(defn new-node-helper
+  [tree-vector]
+  (zip/node (first tree-vector)))
+
+
+(defn new-root-helper
+  [tree-vector]
+  (zip/root (first tree-vector)))
+
+
+(fact "move-up-safely works as intended"
+  (zip/node little-tree) => 99
+  (zip/node (move-up-safely little-tree)) => '(99)
+  (zip/node (move-up-safely
+              (move-up-safely little-tree))) => '(3 4 (99))
+  (zip/node (move-up-safely
+              (move-up-safely
+                (move-up-safely little-tree)))) => '(3 4 (99))
+  (zip/node (move-up-safely
+              (move-up-safely
+                (move-up-safely
+                  (move-up-safely little-tree))))) => '(3 4 (99)))
+
+
+(fact "close-up-one does nothing if the branch-stack is empty"
+  (new-node-helper (close-up-one [little-tree '()])) => 99
+  (new-node-helper (close-up-one [(zip/up little-tree) '()])) => '(99))
+
+
+
+(fact "close-up-one pops one item off the branch-stack if possible"
+  (second (close-up-one [little-tree '()])) => '()
+  (second (close-up-one [little-tree '(:END)])) => '()
+  (second (close-up-one [little-tree '(:END :foo)])) => '(:foo))
+
+
+(fact "close-up-one moves the cursor up a level (until root) if the branch-stack ends in :END"
+  (new-node-helper (close-up-one [little-tree '(:END)])) => '(99)
+  (new-node-helper (close-up-one [(zip/up little-tree) '(:END)])) => '(3 4 (99)))
+
+
+
+(fact "close-up-one moves the cursor up a level (until root) and starts a sibling if the branch-stack ends in :BRANCH"
+  (new-root-helper (close-up-one [little-tree '(:BRANCH)])) => '(1 2 (3 4 (99) ()))
+  (new-node-helper (close-up-one [little-tree '(:BRANCH)])) => nil
+  
+  (new-root-helper
+    (close-up-one
+      [(zip/up little-tree) '(:BRANCH)])) => '(1 2 (3 4 (99)) ())
+  (new-node-helper
+    (close-up-one
+      [(zip/up little-tree) '(:BRANCH)])) => nil)
+
+
+
+
+
+
 
