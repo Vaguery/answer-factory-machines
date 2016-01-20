11939f6f4ac5213441b4aadfcc8f295f24eb8d29
Bill Tozier <bill@vagueinnovation.com>
2016-01-20 07:18:49 -0500

    fixed bug involving close-up-one

--- src/answer_factory/genomes/plush.clj
+++ src/answer_factory/genomes/plush.clj
@@ -3,9 +3,8 @@
   (:require [answer-factory.genomes.bb8 :as bb8]))
 
 
-
 (defn clean-insert
-  "inserts the given item at the current cursor position in the indicated zipper; if the cursor is in an empty sub-list, it removes the `nil` placeholder"
+  "takes a zipper, and inserts the given item at the current cursor position; if the cursor is currently in an empty sub-list, it does so by deleting the `nil` placeholder; cursor then moves to the inserted item"
   [z item]
   (if (nil? (zip/node z))
     (zip/replace z item)
@@ -13,24 +12,19 @@
 
 
 (defn empty-program
-  "returns a seq-zip on an empty list, with the cursor advanced into the list"
+  "returns a seq-zip built on an empty list, with the cursor advanced into the list"
   []
   (zip/next (zip/seq-zip '())))
 
 
 (defn append-token
+  "takes a zipper and any item; inserts the item to the right of the current cursor position, or replaces the current cursor if it is in an empty sub-list; cursor moves to the insered item"
   [z item]
   (clean-insert z item))
 
 
-(defn append-as-branch
-  [z item]
-  (-> (clean-insert z '())
-      zip/next
-      (clean-insert item)))
-
-
 (defn move-up-safely
+  "takes a zipper; moves the cursor up one level towards the root, or stays in the root if already there; then moves to the rightmost position in that new level"
   [program]
   (if (bb8/root? (zip/up program))
       (zip/rightmost program)
@@ -39,6 +33,7 @@
 
 
 (defn append-as-sibling
+  "takes a zipper and an item; inserts the item as the 'cousin' of the current cursor position, moving up (bounded by root), by a new list to the right of the current sub-list and placing the item inside that new list; cursor moves to the new item"
   [z item]
   (-> (move-up-safely z)
       (clean-insert '())
@@ -46,42 +41,82 @@
       (clean-insert item)))
 
 
-
-
-; - increment close-count
-; - (noop_open-paren)
-;   - create a branch (push :END)
-; - noop_delete_prev_paren_pair
-;   - ?
-; - item is in branching table
-;   - push new branch stuff onto branch stack
-; - append an item
-; - consume closes if (pos? close-count)
-;   - move up, consuming branches, that many times
-;   - e.g. [:END :BRANCH :END]
-
-
-; 1. check for tree-editing instructions
-; 2. implement branching items
-; 3. implement boring items
-; 4. do "closing"
-
-
-
+(defn begin-branch
+  "takes a zipper; adds a new sub-list at the right of the current cursor position, and moves into that empty sub-list"
+  [z]
+  (-> (clean-insert z '())
+      zip/down))
 
 
 (defn close-up-one
+  "takes a tuple composed of a program (zipper) and a branch-stack (seq) from which it pops an item; if the branch-stack is empty, it returns the tuple unchanged; if the popped item is `:AGAIN` it moves the cursor to an empty 'sibling' sub-tree it creates; if `:END` it simply moves up a level"
   [[program branch-stack]]
   (let [outcome (first branch-stack)]
-    (vector
-      (cond
-        (nil? outcome) program
-        (= outcome :BRANCH)
-          (-> (move-up-safely program) (zip/insert-right '()) zip/right zip/down)
-        :else (move-up-safely program))
-    (drop 1 branch-stack))))
+    (cond
+      (nil? outcome) [program branch-stack]
+      (= outcome :AGAIN)
+        [(-> (move-up-safely program) begin-branch)
+         (drop 1 branch-stack)]
+      :else
+        [(move-up-safely program)
+         (drop 1 branch-stack)]
+         )))
 
 
+(defn close-up-n
+  "takes a tuple composed of a program (zipper) and branch-stack (seq), plus a counter; iterates `close-up-one`, returning the resulting altered tuple"
+  [[program branch-stack] n]
+  (loop [state [program branch-stack]
+         counter n]
+    (if (zero? counter)
+      state
+      (recur (close-up-one state)
+             (dec counter)))))
+
+
+(defn push-n-branches
+  "takes a branch-stack (seq) and a counter, and pushes that many copies of `:AGAIN` onto the stack"
+  [stack n]
+  (concat (repeat n :AGAIN) stack))
+
+
+(defn apply-one-gene-to-state
+  "takes a tuple composed of a program (zipper) and branch-stack (seq), a single Plush gene (map) and a branch-map (map) which contains information about how many branches should be opened for particular gene items; if the gene's `:silent` field evaluates to true, it does nothing"
+  [[program branch-stack] gene branch-map]
+  (let [item         (:item gene)
+        close-nows   (:close gene)
+        branches     (or (get branch-map item) 0)
+        silent?      (:silent gene)]
+    (if silent?
+      [program branch-stack]
+      (-> (cond
+            (= item :noop_open_paren)
+              [(begin-branch program) branch-stack]
+            (pos? branches)
+              [(-> program
+                   (append-token item)
+                   begin-branch)
+               (-> branch-stack
+                   (conj :END)
+                   (push-n-branches (dec branches)))]
+            :else
+              [(append-token program item) branch-stack])
+          (close-up-n , close-nows))
+      )))
+
+
+(defn plush->push
+  "takes a genome (collection of Plush gene maps) and a branch-map (map), and creates a complete Push program based on the specified gene values and the branch-map directives"
+  [genome branch-map]
+  (loop [program   (empty-program)
+         gene      (first genome)
+         remainder (rest genome)
+         stack     (list)]
+    (if (nil? gene)
+      (into [] (zip/root (first (close-up-n [program stack] (count stack)))))
+      (let [[p s] (apply-one-gene-to-state
+                    [program stack] gene branch-map)]
+        (recur p (first remainder) (rest remainder) s)))))
 
 
 
--- test/answer_factory/genomes/plush_test.clj
+++ test/answer_factory/genomes/plush_test.clj
@@ -6,13 +6,13 @@
   (:use clojure.pprint))
 
 
-;; noop_open_paren
-;; noop_delete_prev_paren_pair
-;; :parentheses metadata
-;; :close values
-;; :close-open values
-;; :close count
-;; :silent metadata
+;; [+] noop_open_paren
+;; [ ] noop_delete_prev_paren_pair
+;; [+] :parentheses metadata
+;; [+] :close values
+;; [+] :close-open values
+;; [+] :close count
+;; [+] :silent metadata
 
 
 (fact "clean-insert adds a thing to a zipper without leaving the nil placeholder in an empty sub-list"
@@ -40,24 +40,15 @@
       '(1 2 (3 4 99)))
 
 
-(fact "append-as-branch adds an item to the program in a new branch"
-  (zip/root
-    (append-as-branch (empty-program) 99)) => '((99))
-  (zip/root
-    (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 99)) =>
-      '(1 2 (3 4 (99))))
-
-
 (fact "append-as-sibling adds an item to the program in a new branch"
   (zip/root
     (append-as-sibling
-      (append-as-branch (empty-program) 88)
+      (bb8/fast-forward (zip/seq-zip '((88))))
       99)) => '((88) (99))
   (zip/root
     (append-as-sibling
-      (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 88)
-      99)) =>
-      '(1 2 (3 4 (88) (99))))
+      (bb8/fast-forward (zip/seq-zip '(1 2 (3 4 (88)))))
+      99)) => '(1 2 (3 4 (88) (99))))
 
 
 ;; genes
@@ -65,16 +56,7 @@
 ;; fixture
 
 (def little-tree
-  (append-as-branch (bb8/fast-forward (zip/seq-zip '(1 2 (3 4)))) 99))
-
-
-(fact "a gene with no closes and an empty branch stack"
-  (zip/root
-    (apply-gene [(empty-program) []] {:item :foo :close 0} {})) =>
-      '(:foo)
-  (zip/root
-    (apply-gene [little-tree []] {:item :foo :close 0} {})) =>
-      '(1 2 (3 4 (99 :foo))))
+  (bb8/fast-forward (zip/seq-zip '(1 2 (3 4 (99))))))
 
 
 (defn new-node-helper
@@ -101,6 +83,13 @@
                   (move-up-safely little-tree))))) => '(3 4 (99)))
 
 
+(fact "begin-branch"
+  (zip/root (begin-branch (empty-program))) => '(())
+  (zip/node (begin-branch (empty-program))) => nil
+  (zip/root (begin-branch little-tree)) => '(1 2 (3 4 (99 ())))
+  (zip/node (begin-branch little-tree)) => nil)
+
+
 (fact "close-up-one does nothing if the branch-stack is empty"
   (new-node-helper (close-up-one [little-tree '()])) => 99
   (new-node-helper (close-up-one [(zip/up little-tree) '()])) => '(99))
@@ -119,20 +108,273 @@
 
 
 
-(fact "close-up-one moves the cursor up a level (until root) and starts a sibling if the branch-stack ends in :BRANCH"
-  (new-root-helper (close-up-one [little-tree '(:BRANCH)])) => '(1 2 (3 4 (99) ()))
-  (new-node-helper (close-up-one [little-tree '(:BRANCH)])) => nil
+(fact "close-up-one moves the cursor up a level (until root) and starts a sibling if the branch-stack ends in :AGAIN"
+  (new-root-helper (close-up-one [little-tree '(:AGAIN)])) => '(1 2 (3 4 (99) ()))
+  (new-node-helper (close-up-one [little-tree '(:AGAIN)])) => nil
   
   (new-root-helper
     (close-up-one
-      [(zip/up little-tree) '(:BRANCH)])) => '(1 2 (3 4 (99)) ())
+      [(zip/up little-tree) '(:AGAIN)])) => '(1 2 (3 4 (99)) ())
   (new-node-helper
     (close-up-one
-      [(zip/up little-tree) '(:BRANCH)])) => nil)
+      [(zip/up little-tree) '(:AGAIN)])) => nil)
+
+
+(fact "close-up-n iterates close-up-one on the state n times"
+  (close-up-n [little-tree '(:END :END :END)] 0) => [little-tree '(:END :END :END)]
+  (close-up-n [little-tree '(:END :END :END)] 1) =>
+    (close-up-one [little-tree '(:END :END :END)])
+  (close-up-n [little-tree '(:END :END :END)] 2) =>
+    (close-up-one (close-up-one [little-tree '(:END :END :END)]))
+  (close-up-n [little-tree '(:END :END :END)] 3) =>
+    (close-up-one (close-up-one (close-up-one [little-tree '(:END :END :END)])))
+  )
+
+
+(fact "close-up-n exhausts the branch-stack eventually"
+  (second (close-up-n [little-tree '(:END :END :END)] 3)) => '())
+
+
+(fact "close-up-n stops changing things when it's run out of branch-stack items"
+  (close-up-n [little-tree '(:END :END :END)] 99) =>
+    (close-up-n [little-tree '(:END :END :END)] 3))
+
+
+(fact "close-up-n works with branches"
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN)] 1)) =>
+      '(1 2 (3 4 (99) ()))
+  (second
+    (close-up-n [little-tree '(:AGAIN)] 1)) =>
+      '()
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN)] 10)) =>
+      '(1 2 (3 4 (99) ()))
+
+
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN :END :END)] 6)) =>
+      '(1 2 (3 4 (99) ()))
+
+
+  (new-root-helper
+    (close-up-one [little-tree '(:AGAIN :AGAIN :END)])) =>
+      '(1 2 (3 4 (99) ()))
+  (new-node-helper
+    (close-up-one [little-tree '(:AGAIN :AGAIN :END)])) =>
+      nil
+  (second
+    (close-up-one [little-tree '(:AGAIN :AGAIN :END)])) =>
+      '(:AGAIN :END)
+
+  (new-root-helper
+    (close-up-one 
+      (close-up-one [little-tree '(:AGAIN :AGAIN :END)]))) =>
+      '(1 2 (3 4 (99) () ()))
+  (second
+    (close-up-one 
+      (close-up-one [little-tree '(:AGAIN :AGAIN :END)]))) =>
+      '(:END)
+
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN :AGAIN :END)] 2)) =>
+      '(1 2 (3 4 (99) () ()))
+  (second
+    (close-up-n [little-tree '(:AGAIN :AGAIN :END)] 2)) =>
+      '(:END)
+
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN :AGAIN :AGAIN)] 6)) =>
+      '(1 2 (3 4 (99) () () ()))
+  (new-root-helper
+    (close-up-n [little-tree '(:AGAIN :AGAIN :END)] 6)) =>
+      '(1 2 (3 4 (99) () ()))
+  (new-root-helper
+    (close-up-n [little-tree '(:END :AGAIN :END)] 6)) =>
+      '(1 2 (3 4 (99)) ()))
+
+
+;; empty gene
+
+;; simple programs, no branches
+
+;; only branching
 
 
 
+;; apply-one-gene-to-state
+
+(fact "simple gene, no close-nows"
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {})) => '(1 2 (3 4 (99 :foo)))
+  (new-node-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {})) => :foo
+    )
+
+
+(fact "branching gene, no close-nows"
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {:foo 1})) => '(1 2 (3 4 (99 :foo ())))
+  (new-node-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {:foo 1})) => nil
+
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {:foo 6})) => '(1 2 (3 4 (99 :foo ())))
+  (second
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0}
+      {:foo 6})) => '(:AGAIN :AGAIN :AGAIN :AGAIN :AGAIN :END)    
+  )
+
+
+(fact "branching gene, with close-nows"
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 1}
+      {:foo 6})) => '(1 2 (3 4 (99 :foo () ())))
+  (second
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 1}
+      {:foo 6})) => '(:AGAIN :AGAIN :AGAIN :AGAIN :END)   
+  
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 2}
+      {:foo 6})) => '(1 2 (3 4 (99 :foo () () ())))
+  (second
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 2}
+      {:foo 6})) => '(:AGAIN :AGAIN :AGAIN :END)    
+ 
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 99}
+      {:foo 6})) => '(1 2 (3 4 (99 :foo () () () () () ())))
+  (second
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 99}
+      {:foo 6})) => '()    
+  )
+
+
+(fact "silent gene"
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 0 :silent true}
+      {})) => '(1 2 (3 4 (99))))
+
+
+(fact "a silent gene closes no blocks"
+  (new-node-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :foo :close 3 :silent true}
+      {})) => 99)
+
+
+(fact "a silent gene doesn't change the block-stack"
+  (second
+    (apply-one-gene-to-state
+      [little-tree '(:AGAIN :AGAIN :END)]
+      {:item :foo :close 3 :silent true}
+      {:foo 8})) => '(:AGAIN :AGAIN :END))
+
+
+(fact "noop_open_paren gene"
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :noop_open_paren :close 0}
+      {})) => '(1 2 (3 4 (99 ())))
+  (new-root-helper
+    (apply-one-gene-to-state
+      [little-tree '()]
+      {:item :noop_open_paren :close 0 :silent true}
+      {})) => '(1 2 (3 4 (99)))
+
+  )
+
+;; plush->push
+
+(fact "plush->push for empty genomes"
+  (plush->push [] {}) => [])
+
+
+(fact "plush->push for simple genomes"
+  (plush->push [{:item 1 :close 0}
+                {:item 2 :close 0}
+                {:item 3 :close 0}] {}) => [1 2 3])
+
+
+(fact "plush->push for branching genomes"
+  (plush->push [{:item :branches :close 0}
+                {:item 2 :close 0}
+                {:item 3 :close 0}] {:branches 2}) => '[:branches (2 3) ()]
+  (plush->push [{:item :branches :close 1}
+                {:item 2 :close 0}
+                {:item 3 :close 0}] {:branches 2}) => '[:branches () (2 3)]
+  (plush->push [{:item :branches :close 1}
+                {:item 2 :close 1}
+                {:item 3 :close 1}] {:branches 3}) =>
+      '[:branches () (2) (3)]
+  (plush->push [{:item :branches :close 1}
+                {:item 2 :close 1}
+                {:item :branches :close 1}] {:branches 3}) => 
+                  '[:branches () (2) (:branches () () ())])
 
 
 
+;; some acceptance tests & bug fixes
 
+(fact "plush->push with closes up parens right"
+  (plush->push
+    [ {:item :foo :close 0}
+      {:item 1    :close 0}
+      {:item 2    :close 3}
+      {:item 3    :close 0}]
+    {:foo 1}) => '[:foo (1 2) 3])
+
+
+(fact "plush->push example per Tom Helmuth"
+  (plush->push
+    [ {:item :exec-do*times   :close 0}
+      {:item 8                :close 0}
+      {:item 11               :close 3}
+      {:item :integer-add     :close 0 :silent true}
+      {:item :exec-if         :close 1}
+      {:item 17               :close 0}
+      {:item :noop_open_paren :close 0}
+      {:item false            :close 0}
+      {:item :code-quote      :close 0}
+      {:item :float-mult      :close 2}
+      {:item :exec-rot        :close 0}
+      {:item 34.44            :close 0}]
+    { :exec-do*times 1
+      :exec-if       2
+      :code-quote    1
+      :exec-rot      3}) =>
+  '[:exec-do*times (8 11) :exec-if ()
+    (17 (false :code-quote (:float-mult)) :exec-rot (34.44) () ())])
