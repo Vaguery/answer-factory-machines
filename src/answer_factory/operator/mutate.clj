824dceb1604d0a91e2b5db146fd5cb4d9489aff8
Bill Tozier <bill@vagueinnovation.com>
2016-01-29 07:33:46 -0500

    added Plush mutation functions

--- src/answer_factory/operator/mutate.clj
+++ src/answer_factory/operator/mutate.clj
@@ -1,2 +1,32 @@
 (ns answer-factory.operator.mutate
   (:require [answer-factory.answer.push :as answer]))
+
+
+(defn mutate-plush-item
+  "takes a Plush gene map, a probability of change, and a list of replacement items; returns the modified gene with the indicated probability, sampling uniformly from the list"
+  [gene prob item-source]
+  (if (and (< (rand) prob) (not-empty item-source))
+    (let [new-item (rand-nth item-source)]
+      (assoc gene :item new-item))
+    gene))
+
+
+
+(defn binomial-sample
+  "takes a probability value p, and an optional keyword argument :limit; returns a binomial sample with the probability of 'heads' set to p, with a maximum value of :limit + 1"
+  [p & {:keys [limit] :or {limit 20}}]
+  (loop [result 0]
+    (if (or (> (rand) p) (> result limit))
+      result
+      (recur (inc result)))))
+
+
+
+(defn mutate-plush-close
+  "takes a Plush gene map, a probability of resampling (mutation), a binomial coefficient, and a max cutoff; changes the :close field of the gene to a new binomial sample with the indicated probability"
+  [gene prob coefficient cutoff]
+  (if (< (rand) prob)
+    (let [new-close (binomial-sample coefficient :limit cutoff)]
+      (assoc gene :close new-close))
+    gene))
+
--- test/answer_factory/operator/erc_mutate_test.clj
+++ test/answer_factory/operator/erc_mutate_test.clj
@@ -2,3 +2,43 @@
   (:use midje.sweet)
   (:use answer-factory.answer.push)
   (:use answer-factory.operator.mutate))
+
+
+(fact "mutate-plush-item changes the :item field"
+  (mutate-plush-item {:item 22 :close 0} 1.0 [99]) => {:item 99 :close 0}
+  (mutate-plush-item {:item 22 :close 0} 1.0 [false]) => {:item false :close 0}
+  (mutate-plush-item {:item 22 :close 0} 0.0 [false]) => {:item 22 :close 0})
+
+
+(fact "mutate-plush-item does nothing if the list is empty"
+  (mutate-plush-item {:item 22 :close 0} 1.0 []) => {:item 22 :close 0})
+
+
+(fact "mutate-plush-item actually samples the list"
+  (mutate-plush-item {:item 22 :close 0} 1.0 [1 2 3]) => {:item 8888 :close 0}
+    (provided (rand-nth [1 2 3]) => 8888))
+
+
+;; binomial-sample
+
+(fact "binomial-sample has a default cutoff"
+  (binomial-sample 0) => 0
+  (binomial-sample 1) => 21
+  (binomial-sample 0.8) => integer?
+  (binomial-sample 0.8) => #(< % 22))
+
+
+(fact "binomial-sample has an optionally settable cutoff"
+  (binomial-sample 0 :limit 999) => 0
+  (binomial-sample 1 :limit 999) => 1000
+  (binomial-sample 0.8 :limit 999) => integer?
+  (< (binomial-sample 0.999 :limit 666) 668) => true)
+
+
+;; mutate-plush-close
+
+(fact "mutate-plush-close replaces the :close field"
+  (mutate-plush-close {:item 22 :close 0} 1.0 1.0 10) => {:item 22 :close 11}
+  (mutate-plush-close {:item 22 :close 1} 1.0 0.0 55) => {:item 22 :close 0})
+
+
