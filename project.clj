3fdc4892a2ae24ff797ddf475b4f64f0a262f274
Bill Tozier <bill@vagueinnovation.com>
2016-03-16 10:06:20 -0400

    cleaned up project.clj a bit

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject answer-factory "0.0.2-SNAPSHOT"
   :description "A support library for generative programming."
+  :url "https://github.com/Vaguery/answer-factory-clj"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [danlentz/clj-uuid "0.1.6"]
                  [klapaucius "0.1.10-SNAPSHOT"]
